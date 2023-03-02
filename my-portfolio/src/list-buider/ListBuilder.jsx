edbe83e3df3e79c2880fdd79652ea9adab2ca5c5
jannat28-349 <jucse2.349@gmail.com>
2023-03-03 00:00:26 +0600

    Output a given list using ul and map

--- my-portfolio/src/list-buider/ListBuilder.jsx
+++ my-portfolio/src/list-buider/ListBuilder.jsx
@@ -1,13 +1,20 @@
 function ListBuilder() {
+  const list = ["APPLE", "DELL", "ASUS", "HP", "ACER", "SAMSUNG"];
   return (
     <div className="ListBuilder">
       <div className="Input">
         <input />
         <button>Add</button>
       </div>
-      <div className="Output"></div>
+      <div className="Output">
+        <ul>
+          {list.map((item) => {
+            return <li>{item}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export {ListBuilder};
+export { ListBuilder };
