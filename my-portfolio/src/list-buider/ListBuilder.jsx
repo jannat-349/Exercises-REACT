7c722f6cbd57aa5a1301df715be3678fa504da4c
jannat28-349 <jucse2.349@gmail.com>
2023-03-02 23:47:22 +0600

    added BuildList component on separate directory

---
+++ my-portfolio/src/list-buider/ListBuilder.jsx
@@ -0,0 +1,13 @@
+function ListBuilder() {
+  return (
+    <div className="ListBuilder">
+      <div className="Input">
+        <input />
+        <button>Add</button>
+      </div>
+      <div className="Output"></div>
+    </div>
+  );
+}
+
+export {ListBuilder};
