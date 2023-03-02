8e1848912da49184c41e38293354afd367f8fb68
jannat28-349 <jucse2.349@gmail.com>
2023-03-02 23:34:28 +0600

    added multifile in same directory

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,6 @@
+import { Counter } from "./Counter";
 import "./App.css";
-import { useState } from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrease = () => {
-    setCount(count + 1);
-  }
-  const handleDecrease = () => {
-    setCount(count - 1);
-  }
-
-  return (
-    <div className="Counter">
-      <h1>Counter</h1>
-      <p>{count}</p>
-      <button onClick={handleIncrease}>+</button>
-      <button onClick={handleDecrease}>-</button>
-    </div>
-  );
-}
 function App() {
   return (
     <div className="App">
